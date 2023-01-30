46daf35410ea8071f6e28d5be467d4c1cb5fa7b4
Brian <p23bconnolly@fairfieldprep.org>
2023-01-30 08:54:41 -0500

    s

--- lab_10-1.py
+++ lab_10-1.py
@@ -3,10 +3,12 @@
 sum = 0
 
 while True:
+    #while loop that will write program 
     num=int(input('number: '))
     if num== -1:
+        #checks if the user inputs -1
         print(sum)
         break
     sum += num
-
+#break statement will end the program 
 
--- lab_10-3.py
+++ lab_10-3.py
@@ -3,7 +3,7 @@ def double_stuff (stuff):
     for index, var in enumerate(stuff):
             stuff[index] = var*2
     return(stuff)
-
+#a function that takes list as its only parameter
 print()
 
 print(double_stuff([1,5,10]))
@@ -11,3 +11,4 @@ print(double_stuff([1,5,10]))
 print(double_stuff([5,"dog",200]))
 
 print(double_stuff(["cat",6,1.75]))
+#each of these test cases double the values in each list
